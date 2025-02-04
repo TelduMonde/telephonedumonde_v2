b46ea6e84a1eddfe681669e8c0a91a83c0dff3e2
Jessy <you@example.com>
2025-02-04 19:34:17 +0100

    Modif: page legale livraison

--- app/(root)/(legal)/livraison-et-retour/page.tsx
+++ app/(root)/(legal)/livraison-et-retour/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 
 export default function LivraisonRetour() {
@@ -30,29 +31,28 @@ export default function LivraisonRetour() {
         <div className="flex flex-col gap-2">
           <h2 className="text-[2rem] font-bold">2. Frais de Livraison</h2>
           <p className="text-sm">
-            Les frais de livraison sont calculés en fonction du mode de
-            livraison choisi et du montant total de la commande. Les frais de
-            livraison sont indiqués au moment de la validation de la commande.
+            Les frais de livraison sont calculés en fonction du mode choisi et
+            du montant total de la commande. Ils sont indiqués au moment de la
+            validation de votre commande.
           </p>
         </div>
 
         <div className="flex flex-col gap-2">
           <h2 className="text-[2rem] font-bold">3. Délais de Livraison</h2>
           <p className="text-sm">
-            Les délais de livraison indiqués sont des estimations et peuvent
-            varier en fonction des disponibilités des produits et des
-            contraintes logistiques. Nous faisons de notre mieux pour respecter
-            les délais annoncés.
+            Les délais de livraison indiqués sont estimatifs et peuvent varier
+            en fonction des disponibilités des produits et des contraintes
+            logistiques. Nous mettons tout en œuvre pour respecter les délais
+            annoncés.
           </p>
         </div>
 
         <div className="flex flex-col gap-2">
           <h2 className="text-[2rem] font-bold">4. Suivi de Commande</h2>
           <p className="text-sm">
-            Dès que votre commande est expédiée, vous recevrez un email de
-            confirmation avec un numéro de suivi. Vous pourrez suivre
-            l&apos;état de votre livraison sur le site de notre partenaire
-            logistique.
+            Dès l'expédition de votre commande, vous recevrez un email de
+            confirmation avec un numéro de suivi. Vous pourrez suivre l’état de
+            votre livraison sur le site de notre partenaire logistique.
           </p>
         </div>
 
@@ -61,18 +61,18 @@ export default function LivraisonRetour() {
           <p className="text-sm">
             Si vous rencontrez un problème avec la livraison de votre commande
             (retard, colis endommagé, etc.), veuillez contacter notre service
-            client à l&apos;adresse suivante : cf Rubrique CONTACT. Nous nous
-            engageons à trouver une solution rapide et satisfaisante.
+            client via la rubrique CONTACT de notre site. Nous nous engageons à
+            trouver une solution rapide et satisfaisante.
           </p>
         </div>
 
         <div className="flex flex-col gap-2">
           <h2 className="text-[2rem] font-bold">6. Droit de Rétractation</h2>
           <p className="text-sm">
-            Conformément à la législation en vigueur, vous disposez d&aops;un
-            délai de 14 jours à compter de la réception de votre commande pour
-            exercer votre droit de rétractation sans avoir à justifier de motifs
-            ni à payer de pénalités.
+            Conformément à la législation en vigueur, vous disposez d’un délai
+            de 14 jours à compter de la réception de votre commande pour exercer
+            votre droit de rétractation, sans avoir à justifier de motifs ni à
+            payer de pénalités.
           </p>
         </div>
 
@@ -100,8 +100,8 @@ export default function LivraisonRetour() {
         <div className="flex flex-col gap-2">
           <h2 className="text-[2rem] font-bold">8. Conditions de Retour</h2>
           <p className="text-sm">
-            Les produits doivent être retournés dans leur état d&apos;origine,
-            non portés et non lavés, avec toutes les étiquettes attachées. Les
+            Les produits doivent être retournés dans leur état d’origine, non
+            portés et non lavés, avec toutes les étiquettes attachées. Les
             articles retournés qui ne respectent pas ces conditions ne seront
             pas acceptés.
           </p>
@@ -110,8 +110,8 @@ export default function LivraisonRetour() {
         <div className="flex flex-col gap-2">
           <h2 className="text-[2rem] font-bold">9. Frais de Retour</h2>
           <p className="text-sm">
-            Les frais de retour sont à la charge du Client, sauf en cas de
-            produit défectueux ou non conforme. Dans ce cas, le Vendeur prendra
+            Les frais de retour sont à la charge du client, sauf en cas de
+            produit défectueux ou non conforme. Dans ce cas, le vendeur prendra
             en charge les frais de retour.
           </p>
         </div>
@@ -120,12 +120,12 @@ export default function LivraisonRetour() {
           <h2 className="text-[2rem] font-bold">10. Remboursements</h2>
           <p className="text-sm">
             Une fois le retour reçu et inspecté, nous vous enverrons un email
-            pour vous notifier de la réception de votre article retourné. Nous
-            vous informerons également de l&apos;approbation ou du rejet de
-            votre remboursement. Si votre retour est approuvé, votre
-            remboursement sera traité, et un crédit sera automatiquement
-            appliqué à votre carte de crédit ou à votre méthode de paiement
-            initiale, dans un délai de [nombre de jours] jours.
+            confirmant la réception de votre article retourné. Nous vous
+            informerons également de l’approbation ou du rejet de votre
+            remboursement. Si votre retour est approuvé, votre remboursement
+            sera traité et un crédit sera automatiquement appliqué à votre carte
+            de crédit ou à votre méthode de paiement initiale, dans un délai de
+            [nombre de jours] jours.
           </p>
         </div>
 
@@ -133,10 +133,10 @@ export default function LivraisonRetour() {
           <h2 className="text-[2rem] font-bold">1. Modes de Livraison</h2>
           <p className="text-sm">
             Si vous souhaitez échanger un produit, veuillez suivre la procédure
-            de retour et indiquer que vous souhaitez un échange. Une fois votre
+            de retour en précisant votre demande d’échange. Une fois votre
             retour reçu et approuvé, nous vous enverrons le produit de
             remplacement. Les frais de réexpédition pour les échanges sont à la
-            charge du Client.
+            charge du client.
           </p>
         </div>
       </div>
